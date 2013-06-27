77222c5f2ac23d88a3eec2f7692b382dbd289fb4
martinab45 <martinab45@gmail.com>
2013-06-27 08:08:29 -0700

    Initial Commit

---
+++ MBSlideCell/MBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MBAppDelegate.h
+//  MBSlideCell
+//
+//  Created by Martina Brazelton on 6/27/13.
+//  Copyright (c) 2013 Martina Brazelton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
